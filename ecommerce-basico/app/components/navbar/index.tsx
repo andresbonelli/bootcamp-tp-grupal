b60b5249c7010efcd05c8abd96006f0038ed302e
andresbonelli <andresbonellipiano@gmail.com>
2024-10-03 20:18:18 -0300

    navbar links

--- ecommerce-basico/app/components/navbar/index.tsx
+++ ecommerce-basico/app/components/navbar/index.tsx
@@ -1,10 +1,14 @@
 import { useCart } from "@/app/CartContextProvider";
+import Link from "next/link";
 
 export default function Navbar() {
   const { totalItems } = useCart();
   return (
     <nav className=" h-20 w-full flex flex-row justify-center items-center py-5 px-20 bg-pink-400 gap-10">
-      <h1 className="text-3xl">NAVBAR</h1>
+      <Link href="/" className="text-3xl">
+        HOME
+      </Link>
+      <Link href="/products">Products</Link>
       <h1 className="text-xl">productos en el carrito:{totalItems}</h1>
     </nav>
   );
