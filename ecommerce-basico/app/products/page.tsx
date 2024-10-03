09f32c2038fe82aa4166729022384332736f9f14
Santiago Pruyas <spruyas@hotmail.com>
2024-10-03 20:04:49 -0300

    Creacion de componente Card

--- ecommerce-basico/app/products/page.tsx
+++ ecommerce-basico/app/products/page.tsx
@@ -1,4 +1,5 @@
 import { Product } from "../interfaces";
+import Card from "../../componentes/card";
 
 export default async function Products() {
   const res = await fetch("https://fakestoreapi.com/products");
@@ -8,7 +9,12 @@ export default async function Products() {
 
   return (
     <>
-      <h1 className="font-bold text-5xl">PRODUCTS PAGE</h1>
+      <h1 className="font-bold text-5xl text-center my-8">PRODUCTS PAGE</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
+        {products.map((product) => (
+          <Card key={product.id} product={product} />
+        ))}
+      </div>
     </>
   );
 }
